8e32a24e86b80f12933bae47c4baef54f16cfd64
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2025-06-16 22:37:59 +1200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 123; // initialization
+        double y = 3.14;
+        boolean z = false;
+        char symbol = '@';
+        String name = "Vishant";
+
+        System.out.println(x);
+        System.out.println("My number is: " + x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println(name);
+        System.out.println("Hello " + name);
+
+    }
+
+}
