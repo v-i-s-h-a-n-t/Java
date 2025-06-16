384a8989f6ab85e33d0cb1784be71541053d643a
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2025-06-16 22:49:45 +1200

    initial commit

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 123; // initialization
+        int age = 20;
         double y = 3.14;
         boolean z = false;
         char symbol = '@';
@@ -14,7 +15,8 @@ public class Main {
         System.out.println(z);
         System.out.println(symbol);
         System.out.println(name);
-        System.out.println("Hello " + name);
+        System.out.println("Hello I am " + name);
+        System.out.println("My age is " + age);
 
     }
 
