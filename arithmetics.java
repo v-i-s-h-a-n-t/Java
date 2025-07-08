bb5a01a3a73639f6e32fbea2397cb9b59495ea31
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2025-07-08 12:50:16 +1200

    add arithmetics file

---
+++ arithmetics.java
@@ -0,0 +1,20 @@
+public class arithmetics {
+    public static void main(String[] args) {
+
+        double num1 = 10;
+        double num2 = 3;
+        double result;
+
+        result = num1 + num2;
+        System.out.println("Add Result = " + result);
+
+        result = num1 - num2;
+        System.out.println("Subtract Result = " + result);
+
+        result = num1 * num2;
+        System.out.println("Multiply Result = " + result);
+
+        result = num1 / num2;
+        System.out.println("Division Result = " + result);
+    }
+}
